b17381515cf93c20b74d32a083da304279c2721b
David Campbell <45454969+DavidCampbellTech@users.noreply.github.com>
2024-10-11 05:08:12 +0100

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 -- my coolnight colorscheme
 config.colors = {
 	foreground = "#CBE0F0",
-	background = "#011423",
+	background = "#050505",
 	cursor_bg = "#47FF9C",
 	cursor_border = "#47FF9C",
 	cursor_fg = "#011423",
@@ -22,10 +22,9 @@ config.colors = {
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 19
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 
-config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.75
+config.window_background_opacity = 0.90
 config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
